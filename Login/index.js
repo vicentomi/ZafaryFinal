5352ca44211094d300cbc028d11fe29ecf1de4ce
Vicente Aldea <vi.aldea@gmail.com>
2020-01-18 02:26:56 -0300

    commit

--- Login/index.js
+++ Login/index.js
@@ -2,31 +2,23 @@ const email = window.document.querySelector('#email');
 const password = window.document.querySelector('#password');
 const mensaje = window.document.querySelector('#mensaje');
 const boton = window.document.querySelector('#boton');
-boton.addEventListener('click', mostrarDatos);
+boton.addEventListener('click', validar);
 
-function mostrarDatos() {
+function validar() {
+
   const emailValue = email.value;
   const passwordValue = password.value;
-  if (emailValue !== '') {
-     console.log('email: ' + emailValue);
+  
+  if (emailValue == "ejemplo@gmail.com" && passwordValue == '123456') {
+     console.log('Iniciando sesión');
      console.log('password: ' + passwordValue);
      email.value = '';
      password.value = '';
      email.focus();
-
-
-     if (emailValue === 'ejemplo@gmail.com' && passwordValue === '123456') {
-       mensaje.classList = [];
-       mensaje.classList.add('mensaje-invisible');
-       console.log('Iniciando sesión');    
-     } else {
-       mensaje.classList = [];
-       mensaje.classList.add('mensaje-visible');
-       console.log('Email y/o contraseña incorrectos');    
-     }
+     password.focus();
 
      
   } else {
-    console.log('Ingresar datos');
+    console.log('Datos incorrectos');
   }
 }
